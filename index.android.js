81ae392630b1d6f10f31b78cb33afa1b8d1462d7
boris-yegorov-8 <borisyegorov8@gmail.com>
2017-01-19 19:20:10 +0300

    Move styles from index.android.js

--- index.android.js
+++ index.android.js
@@ -7,10 +7,10 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
   View,
 } from 'react-native';
+import styles from './app/styles';
 
 export default class AwesomeProject extends Component {
   render() {
@@ -30,24 +30,5 @@ export default class AwesomeProject extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
